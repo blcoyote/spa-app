e03eba3406e524ed912377b49f0ea2eef66b893f
blcoyote <30732216+blcoyote@users.noreply.github.com>
2023-12-05 16:59:52 +0100

    bump daisyui

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className='container mx-auto flex justify-center'>
         <div className='card card-compact w-96 bg-base-100 shadow-xl'>
           <figure>
-            <img src='https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg' alt='Shoes' />
+            <Image src='https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg' alt='Shoes' />
           </figure>
           <div className='card-body'>
             <h2 className='card-title'>Shoes!</h2>
